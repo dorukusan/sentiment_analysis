cf342414a7362f2c7d3bb58d9b0e2090da42d026
arbuz <h@h>
2024-10-21 16:47:10 +0300

    подлючение датасета, функция исправления опечаток и прочие мелочи

--- main3.py
+++ main3.py
@@ -1,9 +1,40 @@
 from nltk.corpus import stopwords
+from collections import Counter
+from spellchecker import SpellChecker
 import re
 import spacy
+import pandas as pd
 
+
+# Загрузка TSKV файла с данными
+data = pd.read_csv('geo-reviews-dataset-2023.tskv', sep='\t', header=None)
+
+# Установка имен столбцов
+data.columns = ['address', 'name_ru', 'rating', 'rubrics', 'text']
+
+# Фильтр только столбцов с текстом отзыва и оценкой
+data = data[['text', 'rating']]
+
+# Снимаем ограничения вывода таблицы
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.width', None)
+
+# Смотрим на данные, выводим 10 первых строк
+print(data[:10])
 
 nlp = spacy.load("ru_core_news_sm")
+spell = SpellChecker(language='ru')
+
+
+# Исправление опечаток
+def correct_spelling(word):
+    if spell.unknown([word]):
+        corrected_word = spell.candidates(word)
+        if corrected_word:
+            return corrected_word.pop()
+    return word
 
 
 # Токенизация текста
@@ -17,8 +48,9 @@ def tokenize(text):
 def clean_text(text):
     text = re.sub(r'[^\w\s]', '', text)
     stop_words = set(stopwords.words('russian'))
-    text = ' '.join(word for word in text.split() if word not in stop_words)
-    return text
+    filtered_words = [word for word in text.split() if word not in stop_words]
+    corrected_words = [correct_spelling(word) for word in filtered_words]
+    return " ".join(corrected_words)
 
 
 # Лемматизация текста
@@ -30,8 +62,8 @@ def lemmatize_text(cleaned_text):
 # Предварительная обработка текста и заполнение словаря
 def process_text(text, dictionary):
     tokens = tokenize(text)
-    # print(tokens)
     tokenize_text = ', '.join(tokens)
+    # print(tokenize_text)
     cleaned_text = clean_text(tokenize_text)
     # print(cleaned_text)
     lemmatized_text = lemmatize_text(cleaned_text)
@@ -43,21 +75,21 @@ def process_text(text, dictionary):
     return lemmatized_text
 
 
+# Вектор слов
 def vectorize_text(text, dictionary):
     vector = [0] * len(dictionary)
-    for i in range(len(vector)):
-        for token in text.split():
-            if token == dictionary[i]:
-                vector[i] += 1
+    token_counts = Counter(text.split())
+    for i, word in enumerate(dictionary):
+        vector[i] = token_counts.get(word, 0)
     return vector
 
 
 dictionary = []
 
-original_text1 = "Синее небо над головой. Кошка прыгнула на стол."
+original_text1 = "Синее небо над головоц. Кошка прыгнула на стол."
 original_text2 = "Синее небо над головой. Кошка прыгнула."
-process_text(original_text1, dictionary)
-processed_text = process_text(original_text2, dictionary)
+# process_text(original_text1, dictionary)
+processed_text = process_text(original_text1, dictionary)
 print(processed_text)
 # print(process_text(original_text2, dictionary))
 print(*dictionary)

1cada41a9bc993b51480ffbc29e07709b305dceb
arbuz <h@h>
2024-10-21 14:26:42 +0300

    предварительная обработка текста

--- main3.py
+++ main3.py
@@ -3,12 +3,17 @@ import re
 import spacy
 
 
+nlp = spacy.load("ru_core_news_sm")
+
+
+# Токенизация текста
 def tokenize(text):
     text = text.lower()
     tokens = text.split()
     return tokens
 
 
+# Чистка текста
 def clean_text(text):
     text = re.sub(r'[^\w\s]', '', text)
     stop_words = set(stopwords.words('russian'))
@@ -16,23 +21,23 @@ def clean_text(text):
     return text
 
 
-original_text = "Синее небо над головой. Кошка прыгнула на стол."
-tokens = tokenize(original_text)
-print(tokens)
-tokenize_text = ', '.join(tokens)
-
-
-cleaned_text = clean_text(tokenize_text)
-print(cleaned_text)
-
-
-nlp = spacy.load("ru_core_news_sm")
-
-
-# Функция для лемматизации текста
+# Лемматизация текста
 def lemmatize_text(cleaned_text):
     doc = nlp(cleaned_text)
     return " ".join([token.lemma_ for token in doc])
 
 
-print(lemmatize_text(cleaned_text))
+# Предварительная подготовка текста
+def process_text(text):
+    tokens = tokenize(original_text)
+    # print(tokens)
+    tokenize_text = ', '.join(tokens)
+    cleaned_text = clean_text(tokenize_text)
+    # print(cleaned_text)
+    lemmatized_text = lemmatize_text(cleaned_text)
+    # print(lemmatized_text)
+    return lemmatized_text
+
+
+original_text = "Синее небо над головой. Кошка прыгнула на стол."
+print(process_text(original_text))

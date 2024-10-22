d7dfe68b2bf85628a26345b2290d734fe73b620c
arbuz <h@h>
2024-10-22 14:40:37 +0300

    добавление прогресс-бара

--- main3.py
+++ main3.py
@@ -1,48 +1,10 @@
 from nltk.corpus import stopwords
 from collections import Counter
 from sklearn.model_selection import train_test_split
+from console_progressbar import ProgressBar
 import re
 import spacy
 import pandas as pd
-
-
-# Загрузка TSKV файла с данными
-data = pd.read_csv('geo-reviews-dataset-2023.tskv', sep='\t', header=None)
-
-# Установка имен столбцов
-data.columns = ['address', 'name_ru', 'rating', 'rubrics', 'text']
-
-# Фильтр только столбцов с текстом отзыва и оценкой
-data = data[['text', 'rating']]
-
-# Срез лишних символов
-data['text'] = data['text'].str.slice(5)
-data['rating'] = data['rating'].str.slice(7)
-
-
-# Снимаем ограничения вывода таблицы
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_colwidth', None)
-pd.set_option('display.width', None)
-
-# Смотрим на данные, выводим 10 первых строк
-print(data[:10])
-data = data[:10]
-
-
-# Разделяем данные на обучающую и тестовую выборки
-(train_set, test_set, train_labels, test_labels) = train_test_split(
-    data['text'],
-    data['rating'],
-    test_size=0.3,
-    # random_state=42
-)
-
-# print(train_set)
-# print(test_set)
-# print(train_labels)
-# print(test_labels)
 
 
 nlp = spacy.load("ru_core_news_sm")
@@ -75,14 +37,11 @@ def lemmatize_text(cleaned_text):
 def preprocess_text(text, dictionary):
     tokens = tokenize(text)
     tokenize_text = ', '.join(tokens)
-    # print(tokenize_text)
     cleaned_text = clean_text(tokenize_text)
-    # print(cleaned_text)
     lemmatized_text = lemmatize_text(cleaned_text)
     for token in lemmatized_text.split():
         if token not in dictionary:
             dictionary.append(token)
-    # print(lemmatized_text)
 
     return lemmatized_text
 
@@ -96,18 +55,60 @@ def bag_of_words(text, dct):
     return vector
 
 
+# Загрузка TSKV файла с данными
+data = pd.read_csv('geo-reviews-dataset-2023.tskv', sep='\t', header=None)
+
+# Установка имен столбцов
+data.columns = ['address', 'name_ru', 'rating', 'rubrics', 'text']
+
+# Фильтр только столбцов с текстом отзыва и оценкой
+data = data[['text', 'rating']]
+
+# Срез лишнего
+data['text'] = data['text'].str.slice(5)
+data['rating'] = data['rating'].str.slice(7, -1)
+data = data[data['rating'].isin(['1', '2', '3', '4', '5'])]
+data['rating'] = data['rating'].astype(int)
+data.reset_index(drop=True, inplace=True)
+
+
+# Снимаем ограничения вывода таблицы
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.width', None)
+
+# Смотрим на данные, выводим 10 первых строк
+# print(data[:10])
+data = data[:1000]
+
+
 dictionary = []
+
+
+# Прогресс-бар для удобства
+pb = ProgressBar(total=len(data)-1, prefix='Progress', suffix='Complete', length=50)
+print("Прогресс обработки текста")
+
+for i in range(len(data)):
+    data.loc[i, 'text'] = preprocess_text(data.loc[i, 'text'], dictionary)
+    pb.print_progress_bar(i)
+
+
+# Разделяем данные на обучающую и тестовую выборки
+(train_set, test_set, train_labels, test_labels) = train_test_split(
+    data['text'],
+    data['rating'],
+    test_size=0.3,
+    # random_state=42
+)
 
 # original_text1 = "Синее небо над головоц. Кошка прыгнула на стол."
 # original_text2 = "Синее небо над головой. Кошка прыгнула."
-
-for i in range(len(data)):
-    print(preprocess_text(data.loc[i, 'text'], dictionary))
-
-
 # preprocessed_text = preprocess_text(test_set, dictionary)
 # print(preprocessed_text)
 # # print(process_text(original_text2, dictionary))
 # print(data)
-print("\n\n", *dictionary)
+# print(*dictionary)
+print(f"Количество слов в словаре: {len(dictionary)}")
 # print(*bag_of_words(preprocessed_text, dictionary))
